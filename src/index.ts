83e7be51c92fe83b093042af9006d4f94afd4de3
Filip Lukas Paidar <filiplukas.paidar@gmail.com>
2023-04-03 20:27:57 -0700

    Small changes, merge

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,60 @@ import express from 'express';
 
 const app = express();
 
+import { MongoClient, Db } from 'mongodb';
+
+import session from 'express-session';
+
+import login from './login';
+import LogManager from './logger/logger';
 
 
-app.listen(process.env.PORT ?? 8080);
+async function main() {
+    const logManager: LogManager = LogManager.getInstance();
+    const dblogger = logManager.logger('Init-MongoDB');
+
+    if (!process.env.DB) {
+        throw (new Error("DB URL is missing"));
+    }
+    if (!process.env.PORT || isNaN(parseInt(process.env.PORT))) {
+        throw (new Error("PORT is missing OR invalid."))
+    }
+    if (process.env.ENV == null || (process.env.ENV !== "DEBUG" && process.env.ENV !== "PROD")) {
+        throw (new Error("ENVIRONMENT is missing OR invalid."))
+    }
+
+    dblogger.log("DEBUG", 'waiting on connection');
+
+    let db: Db | null;
+    try {
+        const dbClient = await MongoClient.connect(process.env.DB);
+        dblogger.log("INFO", "Successful db connection");
+        db = dbClient.db('ProWo')
+    } catch (e) {
+        dblogger.log("ERROR", "Could not connect do db.");
+        throw new Error(JSON.stringify(e));
+    }
+
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false }
+    }));
+
+    app.use(express.urlencoded({ extended: false, }))
+    app.use(express.json());
+    app.set('view engine', 'ejs');
+    app.set('views', './views');
+    app.set('view cache', false);
+    app.use(express.static('./public'));
+
+
+    app.use(login(db));
+    app.listen(process.env.PORT, () => {
+        logManager.logger('Express-Server').logSync('INFO', `Server listens on Port localhost:${process.env.PORT}`);
+        console.log(`Server listens on Port localhost:${process.env.PORT}`);
+    });
+}
+
+main();
