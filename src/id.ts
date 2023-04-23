8626016331e3bcba8047128677cc312514002cd5
Barsch2006 <cchr98756@gmail.com>
2023-04-23 20:55:31 +0200

    pdf gen

--- src/id.ts
+++ src/id.ts
@@ -1,4 +1,4 @@
-function id () {
+function id(): string {
 
     let time = Math.floor(new Date().getTime())
 
@@ -6,10 +6,7 @@ function id () {
 
     time -= 1680307200000 // 	Sat Apr 01 2023 00:00:00 GMT+0000
                           //   Fri Mar 31 2023 17:00:00 GMT-0700
-
-    return time * 1_0_000_000 + randomNumber;
-
-
+    return Buffer.from((time * 1_0_000_000 + randomNumber).toString()).toString('base64').replace('=', '');
 }
 
 export default id;
