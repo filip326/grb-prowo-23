3f63567abf41fa687640a08208aff9dac4875a63
Barsch2006 <cchr98756@gmail.com>
2023-04-24 20:05:31 +0200

    update project-creation

--- src/admin.ts
+++ src/admin.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { Db } from 'mongodb';
-import { isTeacher, Teacher } from './types/user';
+import { isTeacher, Student, Teacher, User as IUser } from './types/user';
+import { Project } from './types/project';
 
 export function admin(db: Db): Router {
     const router = Router();
+
+    const Users = db.collection<Teacher | Student | IUser>('users');
+    const Projekte = db.collection<Project>('projekte')
 
     router.get('/admin', async (req, res) => {
         if (!req.isAuthenticated()) {
@@ -25,7 +29,21 @@ export function admin(db: Db): Router {
             })
         }
 
-        res.render('admin')
+        res.render('admin', {
+            projects: (await Projekte.find({}).toArray()).map(async (value) => {
+                let teacher = await Users.findOne({ id: "${value.teacherId}" })
+                return `<div class="choice" id="${value.id}">
+                        <input type="text" placeholder="title" name="title" required value="${value.name}">
+                        <textarea placeholder="description" name="description" required>${value.description}</textarea>
+                        <input type="text" placeholder="minGrade" name="minGrade" value="${value.minimumGrade}">
+                        <input type="text" placeholder="maxGrade" name="maxGrade" value="${value.maximumGrade}">
+                        <input type="text" placeholder="asignedTeacherUsername" name="asignedTeacher" value="${teacher?.username ?? 'Error'}">
+                        <div class="button-wrapper">
+                            <button onclick="deleteProject('${value.id}')">Löschen</button>
+                        </div>
+                    </div>`;
+            }).join('\n') || "Zur Zeit sind keine Projekte eingetragen"
+        });
 
     });
 
@@ -57,7 +75,7 @@ export function admin(db: Db): Router {
             class: req.user.managedClass,
             grade: req.user.managedGrade,
         })
-    
+
     });
 
     router.post('/my-class', async (req, res) => {
@@ -65,13 +83,13 @@ export function admin(db: Db): Router {
             res.status(403).send("Forbidden");
             return;
         }
-        
+
         if (!isTeacher(req.user)) {
             res.status(403).send("Forbidden");
             return;
         }
 
-        if (!req.body.class_name || !req.body.class_grade  || isNaN(parseInt(req.body.class_grade))) {
+        if (!req.body.class_name || !req.body.class_grade || isNaN(parseInt(req.body.class_grade))) {
             res.status(400).render('error', {
                 error: {
                     code: 400,
@@ -103,7 +121,21 @@ export function admin(db: Db): Router {
 
         await db.collection<Teacher>('users').updateMany({ id: req.user.id }, { $set: { managedClass: req.body.class_name, managedGrade: parseInt(req.body.class_grade) } });
 
-        res.render('admin');
+        res.render('admin', {
+            projects: (await Projekte.find({}).toArray()).map(async (value) => {
+                let teacher = await Users.findOne({ id: "${value.teacherId}" })
+                return `<div class="choice" id="${value.id}">
+                        <input type="text" placeholder="title" name="title" required value="${value.name}">
+                        <textarea placeholder="description" name="description" required>${value.description}</textarea>
+                        <input type="text" placeholder="minGrade" name="minGrade" value="${value.minimumGrade}">
+                        <input type="text" placeholder="maxGrade" name="maxGrade" value="${value.maximumGrade}">
+                        <input type="text" placeholder="asignedTeacherUsername" name="asignedTeacher" value="${teacher?.username ?? 'Error'}">
+                        <div class="button-wrapper">
+                            <button onclick="deleteProject('${value.id}')">Löschen</button>
+                        </div>
+                    </div>`;
+            }).join('\n') || "Zur Zeit sind keine Projekte eingetragen"
+        });
 
     })
 
